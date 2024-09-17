2befa22d6f49bc545875ae0fe8f227722d19a421
alanvillagrand <116222100+alanvillagrand@users.noreply.github.com>
2024-09-17 13:12:04 -0500

    Commit test

--- pet-adoption-api/src/main/java/petadoption/api/PetAdoptionApplication.java
+++ pet-adoption-api/src/main/java/petadoption/api/PetAdoptionApplication.java
@@ -3,6 +3,8 @@ package petadoption.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Commit Test - Alan
+
 @SpringBootApplication
 public class PetAdoptionApplication {
 	public static void main(String[] args) {
