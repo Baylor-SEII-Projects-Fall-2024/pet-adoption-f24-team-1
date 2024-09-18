893fdd0715f6671fbde68aae3c6a03ac3efd8ded
alanvillagrand <116222100+alanvillagrand@users.noreply.github.com>
2024-09-18 17:03:13 -0500

    Register Account Button function added

--- pet-adoption-frontend/src/pages/index.jsx
+++ pet-adoption-frontend/src/pages/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import { Button, Card, CardContent, Stack, Typography } from '@mui/material'
 import styles from '@/styles/Home.module.css'
 
 // Commit Test - alan
 
 export default function HomePage() {
-  const onButtonPress = () => {
-    alert('this will take you to the Register User Account Page');
+  const router = useRouter();
+
+  const onButtonPress = (page) => {
+    router.push(page);
   }
 
   return (
@@ -26,7 +29,7 @@ export default function HomePage() {
           </Card>
           <Stack direction="row">
             {/* There are multiple ways to apply styling to Material UI components. One way is using the `sx` prop: */}
-            <Button variant='contained' onClick={onButtonPress} sx={{ width: 200 }}>Register Account :)</Button>
+            <Button variant='contained' onClick={() => onButtonPress('/RegisterUserAccountPage')} sx={{ width: 200 }}>Register Account :)</Button>
 
             {/* Another way is by creating a dedicated CSS file and using the styles from there: */}
             <Button variant='contained' color="secondary" onClick={onButtonPress} className={styles.wideButton}>I am a wider button</Button>
