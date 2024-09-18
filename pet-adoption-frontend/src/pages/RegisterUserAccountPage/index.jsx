60040b7895ce957e4f4968d9fce37d050a49d59a
Noah Caldwell <noah_caldwell2@baylor.edu>
2024-09-18 16:33:56 -0500

    Added Register Account Page

---
+++ pet-adoption-frontend/src/pages/RegisterUserAccountPage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head'
+
+import styles from '@/styles/Home.module.css'
+
+export default function RegisterUserAccountPage() {
+}
+return(
+    <>
+        <Head>
+            <title>Register User Account Page</title>
+        </Head>
+)
