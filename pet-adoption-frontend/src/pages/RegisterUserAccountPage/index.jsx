b57931c9624d31b483f963abfe6c6d18c99b66b7
Noah Caldwell <noah_caldwell2@baylor.edu>
2024-09-18 16:42:39 -0500

    THIS WILL BE MINECRAFT IN 2030

--- pet-adoption-frontend/src/pages/RegisterUserAccountPage/index.jsx
+++ pet-adoption-frontend/src/pages/RegisterUserAccountPage/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Head from 'next/head'
-
+import { Button, Card, CardContent, Stack, Typography } from '@mui/material'
 import styles from '@/styles/Home.module.css'
 
 export default function RegisterUserAccountPage() {
-}
 return(
     <>
         <Head>
             <title>Register User Account Page</title>
         </Head>
-)
+    </>
+);
+}
--- pet-adoption-frontend/src/pages/index.jsx
+++ pet-adoption-frontend/src/pages/index.jsx
@@ -7,7 +7,7 @@ import styles from '@/styles/Home.module.css'
 
 export default function HomePage() {
   const onButtonPress = () => {
-    alert('You pressed a button!');
+    alert('this will take you to the Register User Account Page');
   }
 
   return (
@@ -26,7 +26,7 @@ export default function HomePage() {
           </Card>
           <Stack direction="row">
             {/* There are multiple ways to apply styling to Material UI components. One way is using the `sx` prop: */}
-            <Button variant='contained' onClick={onButtonPress} sx={{ width: 200 }}>I am a button</Button>
+            <Button variant='contained' onClick={onButtonPress} sx={{ width: 200 }}>Register Account :)</Button>
 
             {/* Another way is by creating a dedicated CSS file and using the styles from there: */}
             <Button variant='contained' color="secondary" onClick={onButtonPress} className={styles.wideButton}>I am a wider button</Button>
