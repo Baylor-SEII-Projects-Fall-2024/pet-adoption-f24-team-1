f277d80d69bc7bee83429a7787f2054b5ca7fe10
Joseph Zuniga <josephzuniga1993@gmail.com>
2024-10-01 14:30:00 -0500

    Made and implemented the Pet class

---
+++ pet-adoption-api/src/main/java/petadoption/api/pet/Pet.java
@@ -0,0 +1,25 @@
+package petadoption.api.pet;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "pet")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int petID;
+    private String petName;
+    private String petBreed;
+    private String petGender;
+    private int petAge;
+    private int petWeight;
+    private String petSpecies;
+    private String color;
+}
