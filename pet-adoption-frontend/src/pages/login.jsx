c657a8076f1bc7a22212735fadbedfba9a9a4e66
nameofme01 <69026352+nameofme01@users.noreply.github.com>
2024-10-01 00:37:06 -0500

    Revert ">merge conflict: accept all both"
    This reverts commit 796416a67f0d2b6659e714e309877c9701dce7c8.

--- pet-adoption-frontend/src/pages/login.jsx
+++ pet-adoption-frontend/src/pages/login.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import Head from 'next/head'
-import { Button, Card, CardContent, Stack, Typography } from '@mui/material'
+import { Button, Card, CardContent, Stack, Typography, Grid, TextField, Container, Link } from '@mui/material'
+import styles from '@/styles/Home.module.css'
+import { useState } from 'react'
+import axios from 'axios';
 
 export default function Login() {
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:8080/api/login", {
+      emailAddress: emailAddress,
+      password: password,
+    })
+            .then(response => {
+                alert("Login successful!");
+                console.log(response.data); // Handle authentication state here
+            })
+            .catch(error => {
+                alert("Login failed: " + error.message);
+            });
+  };
 
   return (
     <>
@@ -11,9 +32,31 @@ export default function Login() {
       </Head>
 
       <main>
-        <Stack sx={{ paddingTop: 4 }} alignItems='center' gap={2}>
-          <p>Login Page</p>
-        </Stack>
+      <Container>
+            <Stack>
+              <Typography variant="h4">
+                Welcome back!
+              </Typography>
+              <Typography>
+                Don't have an account? <Link href="/create-account" variant='body2'>Sign up</Link>
+              </Typography>
+
+              <form onSubmit={handleSubmit}>
+                <Grid container spacing={4} sx={{ my: 1}}>
+                  <Grid item xs={12}>
+                    <TextField id="emailAddress" placeholder="Enter email address" label="Email Address" variant="outlined" fullWidth required onChange={(e) => setEmailAddress(e.target.value)} />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField id="password" placeholder="Enter password" label="Password" variant="outlined" fullWidth required onChange={(e) => setPassword(e.target.value)} />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button type="submit" variant="contained" color="primary" fullWidth>Login</Button>
+                  </Grid>
+                </Grid>
+              </form>
+            </Stack>
+          
+        </Container>
       </main>
     </>
   );
