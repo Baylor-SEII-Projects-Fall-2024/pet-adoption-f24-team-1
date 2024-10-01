d77a6db3e631101f9f6ab4cb69523a52a5763f08
alanvillagrand <116222100+alanvillagrand@users.noreply.github.com>
2024-10-01 08:07:27 -0500

    Register email bug fixed (backend)

--- pet-adoption-api/src/main/java/petadoption/api/user/UserDTO.java
+++ pet-adoption-api/src/main/java/petadoption/api/user/UserDTO.java
@@ -1,31 +1,13 @@
 package petadoption.api.user;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class UserDTO {
-    private String email;
+    private String emailAddress;
     private String password;
     private String userType;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUserType() {
-        return userType;
-    }
-
-    public void setUserType(String userType) {
-        this.userType = userType;
-    }
 }
