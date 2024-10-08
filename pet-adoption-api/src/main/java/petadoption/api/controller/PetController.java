b559c4cea2b2a4d532bb82d7dd7bc21fdb28e351
alanvillagrand <116222100+alanvillagrand@users.noreply.github.com>
2024-10-07 22:55:21 -0500

    API for adding and deleting

--- pet-adoption-api/src/main/java/petadoption/api/controller/PetController.java
+++ pet-adoption-api/src/main/java/petadoption/api/controller/PetController.java
@@ -1,23 +1,40 @@
 package petadoption.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import petadoption.api.pet.Pet;
 import petadoption.api.pet.PetService;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/pets")
 @RestController
 public class PetController {
 
     @Autowired
     private PetService petService;
 
-    @CrossOrigin(origins = "http://localhost:3000")
-    @GetMapping("/api/pets")
+    @GetMapping
     public List<Pet> getAllPets() {
         return petService.getAllPets();
+    }
+
+    @PostMapping
+    public ResponseEntity<Pet> addPet(@RequestBody Pet pet) {
+        Pet createdPet = petService.savePet(pet);
+        return new ResponseEntity<>(createdPet, HttpStatus.CREATED);
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/{id}")
+    public ResponseEntity<String> deletePet(@PathVariable Long id) {
+        boolean isRemoved = petService.deletePet(id);
+        if (!isRemoved) {
+            return new ResponseEntity<>("Pet not found", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>("Pet deleted successfully", HttpStatus.OK);
     }
 }
--- pet-adoption-api/src/main/java/petadoption/api/pet/PetService.java
+++ pet-adoption-api/src/main/java/petadoption/api/pet/PetService.java
@@ -4,13 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PetService {
 
     @Autowired
     private PetRepository petRepository;
+
     public List<Pet> getAllPets() {
         return petRepository.findAll();
+    }
+
+    public Pet savePet(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    public boolean deletePet(Long id) {
+        Optional<Pet> petOptional = petRepository.findById(id);
+        if (petOptional.isPresent()) {
+            petRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
