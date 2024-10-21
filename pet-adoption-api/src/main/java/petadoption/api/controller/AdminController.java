9057c8833d897d60f1538a1dc4c5f23364edf30f
alanvillagrand <116222100+alanvillagrand@users.noreply.github.com>
2024-10-21 16:01:29 -0500

    Changes to admin controller

--- pet-adoption-api/src/main/java/petadoption/api/controller/AdminController.java
+++ pet-adoption-api/src/main/java/petadoption/api/controller/AdminController.java
@@ -1,17 +1,13 @@
 package petadoption.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import petadoption.api.adoptioncenter.AdoptionCenter;
 import petadoption.api.adoptioncenter.AdoptionCenterRepository;
-import petadoption.api.adoptioncenteradmin.AdminDTO;
 import petadoption.api.adoptioncenteradmin.AdoptionCenterAdmin;
 import petadoption.api.adoptioncenteradmin.AdoptionCenterAdminRepository;
 
-import javax.management.RuntimeErrorException;
 import java.util.List;
 
 @RestController
