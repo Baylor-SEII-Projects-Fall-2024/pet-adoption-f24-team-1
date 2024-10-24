125e8e1f6b0672d86a2e3df182259b9c99716c0d
Noah Caldwell <noah_caldwell2@baylor.edu>
2024-10-24 12:11:28 -0500

    mapped petAdoptionForm backend with gcp i think

--- pet-adoption-api/src/main/java/petadoption/api/controller/petAdoptionFormController.java
+++ pet-adoption-api/src/main/java/petadoption/api/controller/petAdoptionFormController.java
@@ -1,21 +1,22 @@
-package petadoption.api.PetAdoptionForm;
+package petadoption.api.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import petadoption.api.PetAdoptionForm.petAdoptionForm;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/pet-adoption-forms")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "http://35.238.40.26:3000"})
 public class petAdoptionFormController {
 
     private static final Logger logger = LoggerFactory.getLogger(petAdoptionFormController.class);
 
     @Autowired
-    private petAdoptionFormService petAdoptionFormService; // Assume you have a service class
+    private petadoption.api.PetAdoptionForm.petAdoptionFormService petAdoptionFormService; // Assume you have a service class
     @PostMapping()
     public petAdoptionForm submitForm(@RequestBody petAdoptionForm form) {
         logger.info("Received form submission: {}", form);
