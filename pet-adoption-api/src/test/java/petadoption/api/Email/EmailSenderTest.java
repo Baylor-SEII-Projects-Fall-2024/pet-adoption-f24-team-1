7a291183469ce7d7f1be42a985869f411a7b3573
nameofme01 <69026352+nameofme01@users.noreply.github.com>
2024-11-10 20:23:41 -0600

    created email service

---
+++ pet-adoption-api/src/test/java/petadoption/api/Email/EmailSenderTest.java
@@ -0,0 +1,22 @@
+package petadoption.api.Email;
+import org.junit.jupiter.api.Test;
+import petadoption.api.EmailService;
+
+import java.util.Date;
+
+
+public class EmailSenderTest {
+
+    /* String requesteeName,
+       String petName,
+       String adoptionCenter,
+       String emailAddress */
+
+    @Test
+    public void sendingEmailTest(){
+        EmailService.sendPetAdoptedNotificationEmail("Alan VillaVENTI",
+                "Fido", "Center Center", "avillagrand04@gmail.com");
+    }
+
+
+}
