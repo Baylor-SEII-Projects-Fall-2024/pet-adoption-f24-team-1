b0f97e9e406b3e7eea1e90a88454f049e9695945
alanvillagrand <116222100+alanvillagrand@users.noreply.github.com>
2024-11-11 16:30:38 -0600

    Moved animal-breeds to proper folder

---
+++ pet-adoption-frontend/src/components/constants/animal-breeds.js
@@ -0,0 +1,47 @@
+
+export const AnimalBreeds = {
+    DOG_BREEDS: [
+        "Labrador Retriever",
+        "German Shepherd",
+        "Golden Retriever",
+        "Bulldog",
+        "Beagle",
+        "Poodle",
+        "Rottweiler",
+        "Yorkshire Terrier",
+        "Boxer",
+        "Dachshund",
+        "Shih Tzu",
+        "Chihuahua",
+        "Pomeranian",
+        "Australian Shepherd",
+        "Cocker Spaniel",
+        "French Bulldog",
+        "Shiba Inu",
+        "Border Collie",
+        "Siberian Husky",
+        "Great Dane"
+      ],
+      CAT_BREEDS: [
+        "Siamese",
+        "Persian",
+        "Maine Coon",
+        "Ragdoll",
+        "Bengal",
+        "Sphynx",
+        "British Shorthair",
+        "Abyssinian",
+        "Scottish Fold",
+        "Russian Blue",
+        "Birman",
+        "Oriental Shorthair",
+        "Savannah",
+        "American Shorthair",
+        "Norwegian Forest",
+        "Turkish Angora",
+        "Siberian",
+        "Devon Rex",
+        "Himalayan",
+        "Tonkinese"
+      ]
+}
