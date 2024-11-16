04d2060a0a33ab4ec9c976be76f725c032acdab2
alanvillagrand <116222100+alanvillagrand@users.noreply.github.com>
2024-11-15 21:56:27 -0600

    Updated CORS ip for center and event controller

--- pet-adoption-api/src/main/java/petadoption/api/controller/AdoptionCenterController.java
+++ pet-adoption-api/src/main/java/petadoption/api/controller/AdoptionCenterController.java
@@ -10,7 +10,7 @@ import petadoption.api.event.EventService;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:3000", "http://35.238.40.26:3000"})
+@CrossOrigin(origins = {"http://localhost:3000", "http://34.133.251.138:3000"})
 @RestController
 @RequestMapping("/api/adoptioncenters")
 public class AdoptionCenterController {
