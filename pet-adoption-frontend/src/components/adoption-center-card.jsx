c0de1a1448f6cdf6ebff6dd101fd8b58ae389298
Noah Caldwell <noah_caldwell2@baylor.edu>
2024-12-09 12:40:11 -0600

    adding some more stuff

--- pet-adoption-frontend/src/components/adoption-center-card.jsx
+++ pet-adoption-frontend/src/components/adoption-center-card.jsx
@@ -1,68 +1,94 @@
-import React, { useState } from 'react';
-import { Button, Grid, Box, Stack, Typography, Paper, useTheme } from '@mui/material';
+import React from 'react';
+import Head from 'next/head';
+import { Button, Card, CardContent, Stack, Typography, Grid, TextField, Container, Link, Paper, Box, alpha, useTheme, IconButton } from '@mui/material'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { red } from '@mui/material/colors';
+import { useState, useEffect } from'react';
 import PetInfoModal from '@/components/pet-info-modal';
 import LoginModal from './login-modal';
 import axios from 'axios';
-import { useRouter } from 'next/router';
+import { useThemeCust } from '@/utils/ThemeContext';
 
-export default function AdoptionCenterCard({ adoptionCenter, user }) {
+export default function AdoptionCenterCard(props) {
     const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL;
-    const router = useRouter();
+    const { toggleTheme, isDarkMode } = useThemeCust();
 
-    const [open, setOpen] = useState(false);
-    const [isLoginModalOpen, setLoginModalOpen] = useState(false);
-    const theme = useTheme();
-
-    const [liked, setLiked] = useState(false);
-
-
+    const [adoptionCenter, setAdoptionCenter] = useState(null);
 
     const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    const handleClose = () => console.log(setOpen(false));
 
-    // Function to navigate to events page
-    const navigateTo = (page) => {
-        console.log('Navigating to:', page);  // Log the full URL
-        router.push(page);
-    };
 
     return (
         <Box>
             <Paper sx={{ width: 250, marginBottom: 10 }} elevation={3}>
                 <Grid container direction="column" alignItems="center" justifyContent="center" rowGap={2}>
-                    {/* Adoption Center Image */}
-                    <Box component="img" sx={{ width: 250, height: 120, objectFit: "cover" }} src={adoptionCenter.centerImageUrl} alt="Adoption center image" />
 
-                    {/* Adoption Center Info */}
-                    <Stack direction="column" alignItems="center">
-                        <Typography variant="h6" marginBottom={2}>{adoptionCenter.centerName}</Typography>
+
+                    <Stack direction="column" alignItems="center" >
+                        <Typography variant="h6" marginBottom={2}>{props.pet.petName}</Typography>
                         <Stack direction="row" alignItems="center" spacing={1} marginLeft={-3}>
-                            <LocationOnIcon fontSize="11px" sx={{ color: theme.palette.primary.light }} />
-                            <Typography variant="body2">{adoptionCenter.centerAddress}</Typography>
+                            <LocationOnIcon fontSize="11px" sx={{ color: theme.palette.primary.light }}/>
+                            <Typography variant="body2">{adoptionCenter ? adoptionCenter.centerName : 'Home Free'}</Typography>
+                        </Stack>
+                        <Stack direction="row" alignItems="center" spacing={1} >
+
+                            <Link fontSize="11px" href={`https://www.google.com/maps/place/${adoptionCenter ? adoptionCenter.centerAddress : '1311 S 5th St, Waco, TX 76706'}`} target="_blank" rel="noopener noreferrer" sx={{ color: !isDarkMode ? theme.palette.primary.dark : 'white' }}>{adoptionCenter ? adoptionCenter.centerAddress : '1311 S 5th St, Waco, TX 76706'}</Link>
                         </Stack>
                     </Stack>
 
-                    {/* Action Buttons */}
                     <Grid container direction="column" alignItems="center" justifyContent="center" rowGap={2}>
+                        <Stack direction={{ xs: 'column', sm: 'row'}} spacing={2}>
+                            <Stack justifyContent="space-between" direction="row" spacing={1} alignItems="center" sx={{width: 105}}>
+                                <Typography variant="body2" align="left">Gender:</Typography>
+                                <Box sx={{ borderRadius: 1, height: "110%", width: "110%", bgcolor: "white"}}>
+                                    <Box sx={{ borderRadius: 1, height: "100%", width: "100%", bgcolor: alpha(theme.palette.primary.light, 0.30), display: "flex" }} alignItems="center" align="right" justifyContent="center">
+                                        <Typography variant="caption" align="center" color={theme.palette.primary.dark}>{props.pet.petGender}</Typography>
+                                    </Box>
+                                </Box>
+                            </Stack>
+                            <Stack justifyContent="space-between" direction="row" spacing={1} alignItems="center" sx={{width: 105}}>
+                                <Typography variant="body2" align="left">Age:</Typography>
+                                <Box sx={{ borderRadius: 1, height: "110%", width: "110%", bgcolor: "white"}}>
+                                    <Box sx={{ borderRadius: 1, height: "100%", width: "100%", bgcolor: alpha(theme.palette.primary.light, 0.30), display: "flex" }} alignItems="center" align="right" justifyContent="center">
+                                        <Typography variant="caption" align="center" color={theme.palette.primary.dark}>{props.pet.petAge} years</Typography>
+                                    </Box>
+                                </Box>
+                            </Stack>
+                        </Stack>
                         <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
-                            <Button variant="outlined" color="primary" href={adoptionCenter.homepage} target="_blank">
-                                Go to Homepage
-                            </Button>
-                            <Button variant="outlined" color="secondary" onClick={() => {
-                                const eventPageUrl = `/adoption-center-home/adoption-center-events/${adoptionCenter.centerId}`;
-                                navigateTo(eventPageUrl);
-                            }} sx={{ width: 100 }}>
-                                View Events
-                            </Button>
+                            <Stack justifyContent="space-between" direction="row" spacing={1} alignItems="center" sx={{width: 105}}>
+                                <Typography variant="body2" align="left">Breed:</Typography>
+                                <Box sx={{ borderRadius: 1, height: "110%", width: "110%", bgcolor: "white"}}>
+                                    <Box sx={{ borderRadius: 1, height: "100%", width: "100%", bgcolor: alpha(theme.palette.primary.light, 0.30), display: "flex", wordWrap: "break-word" }} alignItems="center" align="right" justifyContent="center">
+                                        <Typography fontSize="10px" align="center" color={theme.palette.primary.dark}>{breed}</Typography>
+                                    </Box>
+                                </Box>
+                            </Stack>
+                            <Stack justifyContent="space-between" direction="row" spacing={1} alignItems="center" sx={{width: 105}}>
+                                <Typography variant="body2" align="left">Weight:</Typography>
+                                <Box sx={{ borderRadius: 1, height: "110%", width: "110%", bgcolor: "white"}}>
+                                    <Box sx={{ borderRadius: 1, height: "100%", width: "100%", bgcolor: alpha(theme.palette.primary.light, 0.30), display: "flex" }} alignItems="center" align="right" justifyContent="center">
+                                        <Typography variant="caption" align="center" color={theme.palette.primary.dark}>{props.pet.petWeight} kg</Typography>
+                                    </Box>
+                                </Box>
+                            </Stack>
                         </Stack>
                     </Grid>
+
+                    <Stack sx={{ width: 230, height: 50 }} direction="column" alignItems="center">
+
+                        <Button onClick={handleOpen} sx={{ width: 220, bgcolor: alpha(theme.palette.primary.light, 0.05), color: isDarkMode ? theme.palette.primary.light : theme.palette.primary.main }}variant="outlined">Pet Info</Button>
+
+                    </Stack>
                 </Grid>
             </Paper>
 
-            {/* Modals */}
-            <PetInfoModal open={open} handleClose={handleClose} pet={adoptionCenter} />
-            <LoginModal isOpen={isLoginModalOpen} onClose={() => setLoginModalOpen(false)} />
+
         </Box>
+
+
     );
 }
--- pet-adoption-frontend/src/pages/adoption-center-home/index.jsx
+++ pet-adoption-frontend/src/pages/adoption-center-home/index.jsx
@@ -1,57 +1,87 @@
 import React from 'react';
-import Head from 'next/head'
-import { Button, Box, CardContent, Stack, Typography } from '@mui/material'
-import { useRouter } from 'next/router'
+import Head from 'next/head';
+import { Button, Card, CardContent, Stack, Typography, Grid, TextField, Container, Link, Paper, Box, Pagination } from '@mui/material'
 import styles from '@/styles/Home.module.css'
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router'
+import axios from 'axios';
 import NavBar from '@/components/nav-bar';
-import ProtectedAdminRoute from '@/components/protected-admin-route';
+import FilterStack from '@/components/filter-stack';
+import AdoptionCenterCard from '@/components/adoption-center-card';
+import useAuthUser from 'react-auth-kit/hooks/useAuthUser';
 
 
 export default function AdoptionCenterHome() {
-  const router = useRouter();
+    const user = useAuthUser();
 
-  const navigateTo = (page) => {
-    router.push(page);
-  }
+    const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL;
+    const router = useRouter();
 
-  return (
-    <ProtectedAdminRoute>
-      <Head>
-        <title>Adoption Center Home</title>
-      </Head>
+    const [Name, setName] = useState('');
+    const [Address, setAddress] = useState([]);
+    const [zipCode, setZipCode] = useState(1);
+    const centersPerPage = 20;
 
-    
-      <main>
-        <NavBar />
-        <Stack sx={{ paddingTop: 0 }} alignItems='center' gap={2}>
-        <Box className={styles.container}>
-          <Box className={styles.textContainer}>
-            <Typography variant='h3'>Adoption Center Home Page</Typography>
-            <Box sx={{ display: "flex", justifyContent: "center", width: "100%"}}>
-              <Typography
-                variant="h5"
-                noWrap
-                component="a"
-                sx={{
-                  mr: 2,
-                  display: 'flex',
-                  fontFamily: 'monospace',
-                  fontWeight: 700,
-                  letterSpacing: '.3rem',
-                  color: 'inherit',
-                  textDecoration: 'none',
-                }}
-              >
-              </Typography>
-            </Box>
-            <Typography sx={{ maxWidth: "400px" }}>Here is your landing page for displaying and editing events and pet postings!</Typography>
-          </Box>
+    // Filters
+    const [nameFltr, setNameFltr] = useState('Any');
+    const [addressFltr, setAddressFltr] = useState('Any');
+    const [zipCodeFilter, setZipCodeFltr] = useState([501,99950]);
+
+    function filters(adoptionCenter)  {
+        return (
+            (nameFltr == 'Any' ? true : adoptionCenter.Name == nameFltr) &&
+            (addressFltr == 'Any' ? true : adoptionCenter.address == addressFltr) &&
+            (adoptionCenter.zipCode >= zipCodeFilter[0] && adoptionCenter.zipCode <= zipCodeFilter[1])
+        );
+    }
+
+    // Get location of user
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                    setLocation(position.coords.latitude + ', ' + position.coords.longitude);
+                },
+                (error) => console.log(error.message)
+            );
+        } else {
+            console.log("Geolocation is not supported by this browser");
+        }
+    }, []);
+
+    const handlePageChange = (event, value) => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        setPage(value);
+    };
 
 
-        </Box>
+    return (
+        <>
+            <Head>
+                <title>Home</title>
+            </Head>
 
-        </Stack>
-      </main>
-    </ProtectedAdminRoute>
-  );
+            <main>
+                <Stack spacing={10}>
+                    <NavBar/>
+
+
+                    <Stack direction="row" >
+                        <FilterStack ageFltr={ageFltr} breedFltr={breedFltr} speciesFltr={speciesFltr} weightFltr={weightFltr} distanceFltr={distanceFltr} genderFltr={genderFltr} setAgeFltr={setAgeFltr} setBreedFltr={setBreedFltr} setSpeciesFltr={setSpeciesFltr} setWeightFltr={setWeightFltr} setDistanceFltr={setDistanceFltr} setGenderFltr={setGenderFltr}/>
+
+                        <Grid container direction="row" display="flex" alignItems="center" justifyContent="left" rowGap={2} spacing={2}>
+                            {currentPets.map((pet) => (
+                                <Grid item>
+                                    <AdoptionCenterCard key={adoptionCenter.Name} adoptionCenter={adoptionCenter} user={user} liked={false} location={location} />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Stack>
+
+                    <Stack sx={{ paddingBottom: 10 }} alignItems='center'>
+                        <Pagination count={totalPages} page={page} onChange={handlePageChange}/>
+                    </Stack>
+                </Stack>
+            </main>
+        </>
+    );
 }
